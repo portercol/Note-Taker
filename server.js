928c9894fc9dfd41bc274cf2d3d2414aded9975f
Collin Porter <colls.potta@gmail.com>
2020-10-05 21:56:00 -0600

    removed readfile from get route, required db.json on both get and post routes and push through the new req.body data

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Require express and path modules (npm packages)
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-// const brandNewNote = require("./public/assets/js/index");
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 // Tells node we're creating an express server
 const app = express();
@@ -12,37 +11,42 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Set up middleware to handle data parsing
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+// Create GET & POST requests using an API route
+app.get("/api/notes", function (req, res) {
+    const whatever = require('./db/db.json');
+    res.json(whatever)
+});
+
+
+app.post("/api/notes", function (req, res) {
+    const whatever = require('./db/db.json');
+    const myNewNote = req.body;
+    whatever.push(myNewNote);
+    fs.writeFile("db/db.json", JSON.stringify(whatever), (err) => {
+        if (err) {
+            return myNewNote;
+        }
+    });
+});
 
 // Create HTML GET requests
 // Code below handles when the user visits a page
 // Each case the user is shown a differnt content page
-app.get("/notes", function(req, res){
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("*", function(req, res){
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Create POST requests using an API route
-app.post("/api/notes", function(req, res){
-// Create var and store JSON.stringify and pass thru req.body
-    const myNewNote = JSON.stringify(req.body);
-// Use 'fs' to write new file to db.json, passing thru var created above
-    fs.writeFile("db/db.json", myNewNote, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-    })
-
-
-});
-
 // This code 'starts' the server
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App listening on PORT:" + PORT)
 });
 
